6eeeb7a9de90628585a3f25dfeb9a7b4b06c51a2
celsorv <vitorino.celso@gmail.com>
2021-12-19 22:39:57 -0300

    Developed person create operation

--- src/main/java/com/softhouse/personapi/controller/PersonController.java
+++ src/main/java/com/softhouse/personapi/controller/PersonController.java
@@ -1,16 +1,27 @@
 package com.softhouse.personapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.softhouse.personapi.dto.MessageResponseDTO;
+import com.softhouse.personapi.entity.Person;
+import com.softhouse.personapi.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
 
-    @GetMapping
-    public String getBook() {
-        return "API Test!";
+    private final PersonService personService;
+
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MessageResponseDTO createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
     }
 
 }
